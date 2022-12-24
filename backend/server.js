0c71d40c84db85ba5bb2337c06335e66ef3537c3
Nouar SalhEddine <nouarsalheddine@gmail.com>
2022-12-24 22:07:38 +0100

    cached env

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,28 @@
 require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 //express app 
 
 const app = express()
 
+//middleware
+
+app.use(express.json())
+
 //routes
 
 app.use('/api/workouts', workoutRoutes)
 
-// listen for request 
-
+//connect to db
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  // listen for request 
 app.listen(process.env.PORT, () => {
   console.log("listen to port 4000");
 })
+}).catch((error) => {
+  console.log(error)
+})
+
