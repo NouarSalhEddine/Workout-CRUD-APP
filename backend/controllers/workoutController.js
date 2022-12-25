5f43523c13093b7f9e6b0df37b5413684bac0fe7
Nouar SalhEddine <nouarsalheddine@gmail.com>
2022-12-25 20:35:25 +0100

    first-commit frontend

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -24,6 +24,7 @@ const readWorkout = async (req, res) => {
 
   res.status(200).json(workout);
 };
+
 //Create Workout
 
 const createWorkout = async (req, res) => {
@@ -53,19 +54,22 @@ const deleteWorkout = async (req, res) => {
 // Update Workout
 
 const updateWorkout = async (req, res) => {
-  const  { id } = req.params;
+  const { id } = req.params;
   console.log(mongoose.Types.ObjectId.isValid(id));
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "Not such workout" });
   }
-  const workout = await workoutModel.findOneAndUpdate({_id: id},{...req.body});
+  const workout = await workoutModel.findOneAndUpdate(
+    { _id: id },
+    { ...req.body }
+  );
 
-  res.status(200).json(workout)
+  res.status(200).json(workout);
 };
 module.exports = {
   readWorkouts,
   readWorkout,
   createWorkout,
   deleteWorkout,
-  updateWorkout
+  updateWorkout,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,29 @@
-require('dotenv').config()
-const express = require('express');
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 
-//express app 
+//express app
 
-const app = express()
+const app = express();
 
 //middleware
 
-app.use(express.json())
+app.use(express.json());
 
 //routes
 
-app.use('/api/workouts', workoutRoutes)
+app.use("/api/workouts", workoutRoutes);
 
 //connect to db
-mongoose.set('strictQuery', false)
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  
-app.listen(process.env.PORT, () => {
-  console.log("Connect to dataBase with mongoose");
-})
-}).catch((error) => {
-  console.log(error)
-})
-
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connect to dataBase with mongoose");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
